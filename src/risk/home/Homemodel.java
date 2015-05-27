ab11a6807e9dee0de4c83efd7e33e0f3998bd1ca
Elliott-Green <eg145@brighton.ac.uk>
2015-05-27 22:32:10 +0100

    got images working and buttons for main screens

--- src/risk/home/Homemodel.java
+++ src/risk/home/Homemodel.java
@@ -1,5 +1,20 @@
 package risk.home;
 
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import risk.home.*;
+
 public class Homemodel {
 
+	risk.creategame.CGview..addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent e){
+			
+			Homeview theView = new Homeview();
+			Homemodel theModel = new Homemodel();
+			
+			
+		}
+	});
 }
--- src/risk/home/Homeview.java
+++ src/risk/home/Homeview.java
@@ -1,11 +1,19 @@
 package risk.home;
 
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -18,23 +26,63 @@ public class Homeview extends JFrame {
 	private static final long serialVersionUID = 1L;
 	public static JFrame frame = new JFrame();
 	public static JPanel panel = new JPanel();
+	public static JPanel pic = new JPanel();
+	public static JButton newgame = new JButton("New Game");
+	public static JButton continuegame = new JButton("Continue");
+	public static JButton highscores = new JButton("Highscores");
+
+	
 	
 	public Homeview(){
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.setLayout(new FlowLayout(FlowLayout.CENTER));
-		frame.setLocationRelativeTo(null);
-		frame.setTitle("LOGIN");
-		frame.setSize(1000,1000);
+		frame.setTitle("The Game");
 		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		frame.setSize(950,550);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int) (screenSize.getWidth() / 8);
+		int height = (int) (screenSize.getHeight() / 8);
+		frame.setLocation(width, height);
 		
-//		try {
-//			BufferedImage UoB = ImageIO.read(new File("src/bg1.png").getAbsoluteFile());
-//			JLabel picLabel = new JLabel(new ImageIcon(UoB));
-//			panel.add(picLabel);
-//			}catch (Exception e){
-//				e.printStackTrace();
-//			}
-		frame.add(panel);
-	}
+		
+			try {
+			 BufferedImage  img = ImageIO.read(new File("src/bg1.jpg")/*.getAbsoluteFile()*/);
+			 JLabel piclabel = new JLabel(new ImageIcon(img));
+			 pic.add(piclabel);
+			} catch (IOException e) {
+					e.printStackTrace();
+					//will only fire if picture not located
+			}
+			addButtons();
+			frame.add(pic);
+			frame.show();
+
+			
+}
+
+
+	public static void addButtons() {
+		JPanel panel = new JPanel();
+		
+		panel.add(newgame);
+		panel.add(continuegame);
+		panel.add(highscores);
+		
+	
+		frame.add(newgame);
+		frame.add(continuegame);
+		frame.add(highscores);
+	}}
+	
+	newgame.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent e){
+			frame.dispose();
+			Homeview theView = new Homeview();
+			Homemodel theModel = new Homemodel();
+			
+		}
+	});
+	
+
 }
